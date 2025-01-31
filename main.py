c4ef4613cde564c6a206598b51ac48e7def5228f
shivani-ss12 <saravanan7328@mydusd.org>
2025-01-31 10:10:21 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+two_dee = [[0, 1, 0, 1, 0],[1, 0, 1, 0, 1],[0, 1, 0, 1, 0],[1, 0, 1, 0, 1],[0, 1, 0, 1, 0],[1, 0, 1, 0, 1]]
+for row in two_dee:
+  for col in row:
+    print(col, end = " ") 
+  print() 
